fde873bb5b1a7ca9270a79099d8c2bf06d20eed0
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-05-31 14:18:49 -0300

    Initial

---
+++ portfolio-app/src/components/header/header.jsx
@@ -0,0 +1,15 @@
+import './header.css'
+
+export const Header = () => {
+  return (
+    <div id='home' className="container header-container">
+      <h3>
+        <p > I <span>'</span>m</p>
+        Felipe
+        <h2>Front-end Web Developer</h2>
+      </h3>
+    </div>
+  );
+};
+
+export default Header; 
