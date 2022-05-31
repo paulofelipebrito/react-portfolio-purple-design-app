753a53949ed0fd07ccf85cb568268867670e2037
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-05-31 16:35:45 -0300

    Qualifications

--- portfolio-app/src/components/About/About.jsx
+++ portfolio-app/src/components/About/About.jsx
@@ -1,5 +1,10 @@
 import './About.css';
+import Qualifications from '../Qualifications/Qualifications'
 
 const About = () => {
-  <div id="about" className="container about-container"/>
+  <div id="about" className="container about-container">
+    <Qualifications />
+  </div>
 }
+
+export default About;
